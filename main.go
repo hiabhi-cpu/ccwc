89839fe544355fbc49f57b4b571fd0ee3adc6fac
Abhishek <kalasaraddiabhishek@gmail.com>
2025-05-14 23:44:47 +0530

    added reading form console

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"slices"
 	"strings"
@@ -12,55 +13,72 @@ import (
 )
 
 func main() {
-	// fmt.Println("Hello")
-	if len(os.Args) < 1 {
-		log.Fatal("Usage: ccw <command> [args...]")
-		return
-	}
-	if !strings.Contains(os.Args[1], "-") {
-		if !strings.Contains(os.Args[1], ".") {
-			check(errors.New("Give proper file name"))
-		}
-		//give all outputs
-	}
-	fileName := os.Args[len(os.Args)-1]
-	if !strings.Contains(fileName, ".") {
-		check(errors.New("Give proper file name"))
-	}
-
-	cmdList := make(map[string]func(string))
+	cmdList := make(map[string]func([]byte))
 
 	cmdList["-l"] = lineCommand
 	cmdList["-w"] = wordCommand
 	cmdList["-c"] = charCommand
 	cmdList["-m"] = mCommand
 	cmds := []string{"-l", "-w", "-c", "-m"}
-	argsCmd := os.Args[1 : len(os.Args)-1]
-	if len(argsCmd) == 0 {
-		for _, c := range cmdList {
-			c(fileName)
+
+	if len(os.Args) == 1 {
+		data, err := readFromConsole()
+		check(err)
+		if len(data) == 0 {
+			check(errors.New("Usage: ccw <command> [args...]"))
 		}
+		noCommands(data, cmds, cmdList)
+		fmt.Println()
+	} else {
+		if strings.Contains(os.Args[1], ".") { //has file name
+			fileName := os.Args[len(os.Args)-1]
+			argsCmd := os.Args[1 : len(os.Args)-1]
+			if strings.Contains(os.Args[1], "-") {
+				hasCommands(getDataInBytes(fileName), cmds, argsCmd, cmdList)
+			} else {
+				noCommands(getDataInBytes(fileName), cmds, cmdList)
+			}
+			fmt.Println(fileName)
+		} else { //has no file name
+			data, err := readFromConsole()
+			check(err)
+			if len(data) == 0 {
+				check(errors.New("Usage: ccw <command> [args...]"))
+			}
+			argsCmd := os.Args[1:len(os.Args)]
+			if strings.Contains(os.Args[1], "-") {
+				hasCommands(data, cmds, argsCmd, cmdList)
+			} else {
+				noCommands(data, cmds, cmdList)
+			}
+			fmt.Println()
+		}
+
 	}
+
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func hasCommands(data []byte, cmds, argsCmd []string, cmdList map[string]func([]byte)) {
 	for _, c := range cmds {
 		if slices.Contains(argsCmd, c) {
 			f, e := cmdList[c]
 			if !e {
 				check(errors.New("Give proper command name"))
 			}
-			f(fileName)
+			f(data)
 		}
 	}
-
-	// for _, val := range os.Args[1 : len(os.Args)-1] {
-
-	// }
-
-	fmt.Println(fileName)
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func noCommands(data []byte, cmds []string, cmdList map[string]func([]byte)) {
+	for _, c := range cmdList {
+		c(data)
 	}
 }
 
@@ -70,19 +88,16 @@ func getDataInBytes(fileName string) []byte {
 	return data
 }
 
-func charCommand(fileName string) {
-	data := getDataInBytes(fileName)
+func charCommand(data []byte) {
 	fmt.Print(len(data), "\t")
 }
 
-func lineCommand(fileName string) {
-	data := getDataInBytes(fileName)
+func lineCommand(data []byte) {
 	cnt := strings.Count(string(data), "\n")
 	fmt.Print(cnt, "\t")
 }
 
-func wordCommand(fileName string) {
-	data := getDataInBytes(fileName)
+func wordCommand(data []byte) {
 	cnt := 0
 
 	inward := false
@@ -100,7 +115,12 @@ func wordCommand(fileName string) {
 	fmt.Print(cnt, "\t")
 }
 
-func mCommand(fileName string) {
-	data := getDataInBytes(fileName)
+func mCommand(data []byte) {
 	fmt.Print(utf8.RuneCount(data), "\t")
+}
+
+func readFromConsole() ([]byte, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	return io.ReadAll(reader)
 }
